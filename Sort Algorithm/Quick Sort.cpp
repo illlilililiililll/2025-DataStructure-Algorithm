e0a2090298885b02c152487cf74122745ac5c400
illlilililiililll <kcwrenew22c@gmail.com>
2025-07-20 18:59:26 +0900

    main

--- Sort Algorithm/Quick Sort.cpp
+++ Sort Algorithm/Quick Sort.cpp
@@ -38,14 +38,14 @@ void quickSort(int a[], int n) {
     sort(a, 0, n-1);
 }
 
-//int main() {
-//    int arr[] = {5, 15, 7, 20, 1 ,
-//                 4, 10, 2, 8, 11,
-//                 9, 6, 13, 18, 3,
-//                 16, 12, 14, 17, 19};
-//
-//    quickSort(arr, 20);
-//
-//    for (int i = 0; i < 20; i++)
-//        printf("%d ", arr[i]);
-//}
+int main() {
+    int arr[] = {5, 15, 7, 20, 1 ,
+                 4, 10, 2, 8, 11,
+                 9, 6, 13, 18, 3,
+                 16, 12, 14, 17, 19};
+
+    quickSort(arr, 20);
+
+    for (int i = 0; i < 20; i++)
+        printf("%d ", arr[i]);
+}
